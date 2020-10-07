d874f2181104694ef11f0d247bb6ff55a66c145c
mik-p <mppritchard3@hotmail.com>
2020-10-07 12:44:02 +1100

    add board

---
+++ boards/sensect/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "CSIRO"
+#define PRODUCT_NAME "senseCT-v1"
+#define VOLUME_LABEL "SENSECTBOOT"
+
+#define BOARD_ID "SAMD21G18A-senseCT-v1"
+
+#define USB_VID 0x03EB // atmel
+#define USB_PID 0x2402
+
+#define LED_PIN PIN_PA20
+
+#endif
